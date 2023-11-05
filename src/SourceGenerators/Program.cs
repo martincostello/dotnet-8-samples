6a42210a0829993f61cbcd5d74bf694a74c463c5
martincostello <martin@martincostello.com>
2023-11-05 12:26:47 +0000

    Implement source generator sample
    Implement sample that shows use of request delegate generator and configuration binding source generator.

--- src/SourceGenerators/Program.cs
+++ src/SourceGenerators/Program.cs
@@ -1,41 +1,108 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Options;
 
-builder.Services.AddSingleton(Random.Shared);
-builder.Services.AddSingleton(TimeProvider.System);
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<WeatherClient>();
+builder.Services.AddHttpClient<WeatherClient>(client => client.BaseAddress = new("https://api.open-meteo.com"));
+builder.Services.Configure<WeatherOptions>(builder.Configuration.GetSection("Weather"));
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-
-string[] summaries =
-[
-    "Freezing",
-    "Bracing",
-    "Chilly",
-    "Cool",
-    "Mild",
-    "Warm",
-    "Balmy",
-    "Hot",
-    "Sweltering",
-    "Scorching",
-];
-
-app.MapGet("/weatherforecast", (Random random, TimeProvider timeProvider) =>
+app.MapGet("/forecast", async (
+    WeatherClient client,
+    IOptions<WeatherOptions> options,
+    int? days = null,
+    double? latitude = null,
+    double? longitude = null,
+    string? timeZone = null,
+    WindSpeedUnit? windSpeedUnit = null) =>
 {
-    var now = timeProvider.GetLocalNow();
-    return Enumerable.Range(1, 5)
-        .Select(index =>
-            new WeatherForecast(
-                DateOnly.FromDateTime(now.DateTime.AddDays(index)),
-                random.Next(-20, 55),
-                summaries[random.Next(summaries.Length)]))
-        .ToArray();
+    var defaults = options.Value;
+
+    days ??= defaults.Days;
+    latitude ??= defaults.Location.Latitude;
+    longitude ??= defaults.Location.Longitude;
+    timeZone ??= defaults.TimeZone;
+    windSpeedUnit ??= defaults.WindSpeedUnit;
+
+    var forecast = await client.GetForecastAsync(
+        days.Value,
+        latitude.Value,
+        longitude.Value,
+        windSpeedUnit.Value,
+        timeZone);
+
+    if (forecast is null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Json(forecast);
 });
 
 app.Run();
 
-public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+public class WeatherClient(HttpClient client)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public async Task<WeatherForecast?> GetForecastAsync(
+        int days,
+        double latitude,
+        double longitude,
+        WindSpeedUnit windSpeedUnit,
+        string timeZone,
+        CancellationToken cancellationToken = default)
+    {
+        var requestUri = QueryHelpers.AddQueryString("/v1/forecast", new Dictionary<string, string?>()
+        {
+            ["forecast_days"] = days.ToString(),
+            ["hourly"] = "temperature_2m,wind_speed_10m",
+            ["latitude"] = latitude.ToString(),
+            ["longitude"] = longitude.ToString(),
+            ["timezone"] = timeZone,
+            ["wind_speed_unit"] = windSpeedUnit switch
+            {
+                WindSpeedUnit.Knots => "kn",
+                WindSpeedUnit.MilesPerHour => "mph",
+                WindSpeedUnit.MetersPerSecond => "ms",
+                _ => "",
+            },
+        });
+
+        return await client.GetFromJsonAsync<WeatherForecast>(requestUri, cancellationToken);
+    }
+}
+
+public record HourlyForecast(
+    IList<DateTimeOffset> Time,
+    [property: JsonPropertyName("temperature_2m")] IList<float> Temperature,
+    [property: JsonPropertyName("wind_speed_10m")] IList<float> WindSpeed);
+
+public record WeatherForecast(
+    double Latitude,
+    double Longitude,
+    string Timezone,
+    double Elevation,
+    HourlyForecast Hourly);
+
+public enum WindSpeedUnit
+{
+    KilometersPerHour,
+    Knots,
+    MetersPerSecond,
+    MilesPerHour,
+}
+
+public record struct DefaultLocation(double Latitude, double Longitude);
+
+public class WeatherOptions
+{
+    public int Days { get; set; }
+
+    public DefaultLocation Location { get; set; }
+
+    public string TimeZone { get; set; } = "UTC";
+
+    public WindSpeedUnit WindSpeedUnit { get; set; }
 }
