e80c976346b619cbefeb39b50d8013c08a6ea538
martincostello <martin@martincostello.com>
2023-11-05 11:34:05 +0000

    Tidy up samples
    - Remove redundant files.
    - Fix launch profiles.
    - Suppress warning about namespaces.
    - Use DI.

--- src/SourceGenerators/Program.cs
+++ src/SourceGenerators/Program.cs
@@ -1,31 +1,41 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton(Random.Shared);
+builder.Services.AddSingleton(TimeProvider.System);
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+string[] summaries =
+[
+    "Freezing",
+    "Bracing",
+    "Chilly",
+    "Cool",
+    "Mild",
+    "Warm",
+    "Balmy",
+    "Hot",
+    "Sweltering",
+    "Scorching",
+];
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", (Random random, TimeProvider timeProvider) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
+    var now = timeProvider.GetLocalNow();
+    return Enumerable.Range(1, 5)
+        .Select(index =>
+            new WeatherForecast(
+                DateOnly.FromDateTime(now.DateTime.AddDays(index)),
+                random.Next(-20, 55),
+                summaries[random.Next(summaries.Length)]))
         .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+});
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
